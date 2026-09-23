7cb43a66effff03d24429ed481af02057ac7c0f9
kambreyacc-sketch <kambreyacc@gmail.com>
2026-09-23 10:58:59 -0700

    Reset game data to source-driven entries

---
+++ data/games.ts
@@ -0,0 +1,4 @@
+export type Game={title:string;description:string;image:string;url:string;tag:string};
+
+// Real Spider Studios game entries will be added here from the source site.
+export const games:Game[]=[];
